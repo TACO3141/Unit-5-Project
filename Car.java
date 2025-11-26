127c7b8a42641f2d41c91611120b11ad7c73ffb8
TACO3141 <andrewej10@gmail.com>
2025-11-26 11:31:55 -0700

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,15 @@
+public class Car
+{
+  private double efficiency;
+  private double litres;
+  private double maxFuel;
+  private double minFuel;
+  private double usedFuel;
+
+  public Car(double m)
+  {
+    efficiency = m;
+    litres = 0;
+    
+  }
+}
