55c411b0514ecab4a4ed95cc83198f0492efe856
TACO3141 <andrewej10@gmail.com>
2026-01-06 11:28:12 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,13 @@ public class Main
 {
   public static void main(String[] args)
   {
+    boolean continueLoop = true;
     Scanner scan = new Scanner(System.in);
+    System.out.println("Please input the fuel efficiency value as a decimal.");
+    efficiency = scan.nextDouble();
+    while (continueLoop = true)
+    {
+      System.out.println("Select whether you")
+    }
   }
 }
