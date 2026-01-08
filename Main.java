bd61088ce409ce3cd2a3e5bf1ec4d3b927773ab1
TACO3141 <andrewej10@gmail.com>
2026-01-08 10:38:57 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,13 +4,32 @@ public class Main
 {
   public static void main(String[] args)
   {
+    String input = "";
     boolean continueLoop = true;
     Scanner scan = new Scanner(System.in);
     System.out.println("Please input the fuel efficiency value as a decimal.");
     efficiency = scan.nextDouble();
+    Car car = new Car(efficiency);
     while (continueLoop = true)
     {
-      System.out.println("Select whether you")
+      System.out.println("Select whether you want to FILL, DRIVE or CHECKFUEL.");
+      input = scan.nextLine();
+      if (input.toUpperCase().equals("FILL"))
+      {
+        System.out.println("How many litres do you want to fill up? (Enter a decimal):");
+        input = scan.nextDouble();
+        car.fillUp(input);
+      }
+      else if (input.toUpperCase().equals("DRIVE"))
+      {
+        System.out.println("Enter the distance (decimal):");
+        input = scan.nextDouble();
+        car.takeATrip(input);
+      }
+      else if (input.toUpperCase().equals("CHECKFUEL"))
+      {
+        System.out.println(car.reportFuel());
+      }
     }
   }
 }
