82ae569bf13689d83109099b026482f89cc5e70f
TACO3141 <andrewej10@gmail.com>
2026-01-08 11:13:24 -0700

    Add Tires

--- Car.java
+++ Car.java
@@ -9,6 +9,7 @@ public class Car
   private double maxFuel;
   private double minFuel;
   private double usedFuel;
+  private double tiresAir;
 
   public Car(double m)
   {
@@ -16,9 +17,10 @@ public class Car
     litres = 0;
     maxFuel = 90.0;
     minFuel = 9.0;
+    tiresAir = 100.0;
   }
 
-  public void fillUp(double fill)
+  public void fillUpFuel(double fill)
   {
     if (litres + fill > maxFuel)
     {
@@ -28,7 +30,7 @@ public class Car
     else
     {
       litres += fill;
-      System.out.println("Gas tank filled. Current fuel amount: " + litres + "/" + maxFuel + "L.")
+      System.out.println("Gas tank filled. Current fuel amount: " + litres + "/" + maxFuel + "L.");
     }
   }
     
@@ -39,16 +41,27 @@ public class Car
     {
       System.out.println("You can't drive that distance, you don't have enough fuel (you must keep at least 9L in reserve.)");
     }
-    else
+    else if (tiresAir > 20.0)
     {
       litres = litres - usedFuel;
       System.out.println("You drove " + distance + " KM.");
+      tiresAir -= distance * 0.1;
+      System.out.println("Your tires are now " + tiresAir + "% the recommended tire pressure.");
+    }
+    else
+    {
+      System.out.println("Your tires are dangerously underinflated, you may not drive.");
     }
   }
 
-    public double reportFuel()
-    {
-      return litres;
-    }
-    
+  public double reportFuel()
+  {
+    return litres;
+  }
+
+  public void fillUpTires()
+  {
+    tiresAir = 100;
+    System.out.println("Tires successfully inflated.");
+  }
 }
